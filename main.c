d5ba60fbf15ab72fd9b332bea0606d182a8bd63f
menezesandr3 <menezes.andr3@gmail.com>
2016-09-16 21:45:38 -0300

    programa quase funcional

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main (int argc, char *argv[])
 	char *memoria_alocada;
 	// aloca a quantidade de memoria solicitada (50 x char)
 	// e inicializa o ponteiro com a posicao da memoria
-	memoria_alocada = malloc (50 * sizeof (char));
+	memoria_alocada = calloc (50, sizeof (char));
 	//se o ponteiro for inicializado com o valor nulo
 	if (memoria_alocada == NULL)
 	{
