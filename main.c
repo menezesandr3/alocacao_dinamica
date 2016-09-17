fcafb26ab3b7aa5f1e7a23a41ceee04b073cb98c
menezesandr3 <menezes.andr3@gmail.com>
2016-09-16 21:32:20 -0300

    programa encaminhado

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
+int main (int argc, char *argv[])
+{
+	// cria um ponteiro para o bloco de memoria alocado
+	char *memoria_alocada;
+	// aloca a quantidade de memoria solicitada (50 x char)
+	// e inicializa o ponteiro com a posicao da memoria
+	memoria_alocada = malloc (50 * sizeof (char));
+	//se o ponteiro for inicializado com o valor nulo
+	if (memoria_alocada == NULL)
+	{
+		//exibe a mensagem de erro
+		printf ("Não foi possivel alocar a memoria desejada!\n");
+	}
+	// se o ponteiro nao for nulo
+	else
+	{
+		//transfere a mensagem para a memoria alocada
+		stropy (memoria_alocada, "Esta eh a melhor aula do mundo!\n");
+	}
+	// exibe o conteudo da memoria alocada
+	printf ("O conteudo da memoria alocada eh: %s\n", memoria_alocada);
+	
+	// Libera a memoria alocada
+	free (memoria_alocada);
+	// retorna 0
 	return 0;
 }
